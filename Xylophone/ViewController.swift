4fe02699f1987c2617ee0960bc7596ea41c563c0
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2021-04-05 13:07:33 +0300

    starting files

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,16 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+    }
+    
+}
+
